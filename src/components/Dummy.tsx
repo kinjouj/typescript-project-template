3d32a74536c03bc533ad32184fcfb1c753c610d4
kinjouj <kinjouj@gmail.com>
2025-11-10 04:59:36 +0900

    update

--- src/components/Dummy.tsx
+++ src/components/Dummy.tsx
@@ -1,11 +1,4 @@
-// こうかくとエラーになるように設定してる
-// const Dummy = ({ message }: { message: string }): React.JSX.Element => {
-
-interface DummyProps {
-  message: string
-}
-
-const Dummy = ({ message }: DummyProps): React.JSX.Element => {
+const Dummy = ({ message }: { message: string }): React.JSX.Element => {
   return (
     <h1>{message}</h1>
   );
